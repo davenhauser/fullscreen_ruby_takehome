5d9bd4b45694ba457de6ca3544f3e595906fc6a9
david niederhauser <niederhauser.dave@gmail.com>
2016-05-04 19:02:52 -0700

    Creates actions for index and show for both Videos and Users

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,1 +1,11 @@
-videos_controller.rb
+class VideosController < ApplicationController
+
+  def index
+    @videos = Video.all
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+end
