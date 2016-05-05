60ea5bdc8ca4b04b52387192ed4073fa0b22e959
david niederhauser <niederhauser.dave@gmail.com>
2016-05-04 19:38:05 -0700

    uses Model association to link video and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :videos
   # How does a user relate to a video?
   # You should be able to run the following:
   #    User.last.videos
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ActiveRecord::Base
+
+  belongs_to :user
   # How does a video relate to a user?
   # You should be able to run the following:
   #    Video.last.users
