f670405727df761de53447e033d75151403074bc
david niederhauser <niederhauser.dave@gmail.com>
2016-05-10 16:33:34 -0700

    updates relation between videos and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
-  has_many :videos
+  has_many :users_videos
+  has_many :videos, through: :users_videos
   # How does a user relate to a video?
   # You should be able to run the following:
   #    User.last.videos
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
 
-  belongs_to :user
+  has_many :users_videos
+  has_many :users, through: :users_videos
   # How does a video relate to a user?
   # You should be able to run the following:
   #    Video.last.users
